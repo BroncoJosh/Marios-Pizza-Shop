b2a13414f1077230f2cd812b96250f9f6d176d3b
Josh <joshuawhite1@u.boisestate.edu>
2018-04-09 18:44:08 -0600

    Created HomePage, Created Simple View for Shopping Cart Summary, and navbar placeholder for shopping cart status

---
+++ MariosPizzaShop/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MariosPizzaShop.Models;
+using MariosPizzaShop.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MariosPizzaShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IPizzaRepository _pizzaRepository;
+
+        public HomeController(IPizzaRepository pizzaRepository)
+        {
+            _pizzaRepository = pizzaRepository;
+        }
+        public ViewResult Index()
+        {
+            var homeViewModel = new HomeViewModel
+            {
+                PizzasOfTheWeek = _pizzaRepository.PizzasOfTheWeek
+            };
+            return View(homeViewModel);
+        }
+    }
+}
---
+++ MariosPizzaShop/ViewModels/HomeViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MariosPizzaShop.Models;
+
+namespace MariosPizzaShop.ViewModels
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<Pizza> PizzasOfTheWeek { get; set; }
+    }
+}
